223acf064ac84ce6dc8a800b45987d95939de1b2
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-05-06 16:02:26 +0530

    Time: 1108 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,11 @@
+select  date_format(trans_date, '%Y-%m') as month, country,
+    count(*) AS trans_count,  sum(case
+            when state = 'approved' then 1 
+            else 0 end) as approved_count,
+    
+    sum(amount) AS trans_total_amount,
+    
+    sum(case
+            when state = 'approved' then amount 
+            else 0  end) AS approved_total_amount from Transactions
+group by month, country;
