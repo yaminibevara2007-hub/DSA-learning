f1161c2ec50648687fd0a8d59010ffee5cd1775e
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-05-20 20:54:07 +0530

    Time: 614 ms (28.03%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,6 +1,5 @@
 select round(
-        count(a.player_id) / 
-        (select count(distinct player_id) from Activity),
+        count(a.player_id) /  (select count(distinct player_id) from Activity),
         2) as fraction from Activity a
 join ( select player_id,  min(event_date) as first_login from Activity group by player_id
 ) f on a.player_id = f.player_id and a.event_date = DATE_ADD(f.first_login, INTERVAL 1 day);
