799045388f7374d2477f2139d4bafc0a2b32de42
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-05-22 19:53:23 +0530

    Time: 1294 ms (60.36%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,23 +1,9 @@
-(
-    SELECT u.name AS results
-    FROM Users u
-    JOIN MovieRating mr
-    ON u.user_id = mr.user_id
-    GROUP BY u.user_id
-    ORDER BY COUNT(*) DESC, u.name ASC
-    LIMIT 1
+( select u.name as results from Users u join MovieRating mr on u.user_id = mr.user_id
+    group by u.user_id order by count(*) desc, u.name ASC LIMIT 1
 )
-
 UNION ALL
 
-(
-    SELECT m.title AS results
-    FROM Movies m
-    JOIN MovieRating mr
-    ON m.movie_id = mr.movie_id
-    WHERE mr.created_at >= '2020-02-01'
-      AND mr.created_at < '2020-03-01'
-    GROUP BY m.movie_id
-    ORDER BY AVG(mr.rating) DESC, m.title ASC
-    LIMIT 1
+( select m.title as results from Movies m join MovieRating mr on m.movie_id = mr.movie_id
+    where mr.created_at >= '2020-02-01' and mr.created_at < '2020-03-01'
+     group by m.movie_id order by avg(mr.rating) desc, m.title ASC LIMIT 1
 );
