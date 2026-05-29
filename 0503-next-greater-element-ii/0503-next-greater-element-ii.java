6b0ac33516255aee580e0c32ec145d41c3398882
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-05-29 19:09:45 +0530

    Time: 17 ms (21.26%), Space: 48.2 MB (74.82%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int []ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        Arrays.fill(ans,-1);
+        for(int i= 2* n-1;i>=0;i--){
+            while(!stack.isEmpty()&&stack.peek()<=nums[i%n]){
+                stack.pop();
+            }
+            if(i<n){
+                if(!stack.isEmpty()){
+                ans[i] = stack.peek();
+                }
+            }
+            stack.push(nums[i%n]);
+        }
+        return ans;
+    }
+}
