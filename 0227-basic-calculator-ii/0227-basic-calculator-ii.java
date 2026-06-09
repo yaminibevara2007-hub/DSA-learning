6d0398535dc73e23da0d66a9059c3e149eb01025
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-06-09 19:15:39 +0530

    Time: 26 ms (24.87%), Space: 48.3 MB (42.63%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char sign = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
+
+                if (sign == '+') {
+                    stack.push(num);
+                } else if (sign == '-') {
+                    stack.push(-num);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+
+                sign = ch;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
