c99ccf5da08fddd74bdade735369e148ff3551d5
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-07-28 22:38:55 +0530

    Time: 2 ms (47.35%), Space: 48.7 MB (87.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,50 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        int m = nums1.length;
-        int n = nums2.length;
-
-        int[] arr = new int[m + n];
-
-        int i = 0, j = 0, k = 0;
-
-        // Merge both sorted arrays
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                arr[k++] = nums1[i++];
-            } else {
-                arr[k++] = nums2[j++];
+    double res = merge(nums1 , nums2);
+      
+    return res;
+        
+        
+    }
+    private static double merge(int[] nums1,int[] nums2){
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int p1 = 0;
+        int p2 = 0;
+        List<Integer> ans = new ArrayList<>();
+        while(p1 < n1 && p2 < n2){
+            if(nums1[p1] < nums2[p2]){
+                ans.add(nums1[p1]);
+                p1++;
             }
-        }
+            else{
+                ans.add(nums2[p2]);
+                p2++;
+            }
 
-        while (i < m) {
-            arr[k++] = nums1[i++];
         }
-
-        while (j < n) {
-            arr[k++] = nums2[j++];
+        while(p1 < n1){
+            ans.add(nums1[p1]);
+            p1++;
         }
-
-        int len = m + n;
-
-        if (len % 2 == 0) {
-            return (arr[len / 2] + arr[(len / 2) - 1]) / 2.0;
-        } else {
-            return arr[len / 2];
+        while(p2 < n2){
+            ans.add(nums2[p2]);
+            p2++;
         }
+        double res =0;
+        int temp[] = new int[ans.size()];
+        for(int i =0 ;i < ans.size() ;i++){
+            temp[i] = ans.get(i);
+        }
+        int n = temp.length;
+    if(n % 2 !=0){
+        res = temp[n/2];
+    }
+    else{
+        res = temp[n/2 ] +temp[(n/2) -1];
+        res = res/2;
+    }
+
+        return res;
     }
 }
