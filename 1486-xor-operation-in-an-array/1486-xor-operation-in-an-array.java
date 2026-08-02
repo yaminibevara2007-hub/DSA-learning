e57dddcb1678fcc116c11e718ef36100d15cc3d2
yaminibevara2007-hub <yaminibevara2007@gmail.com>
2026-08-02 20:16:28 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.71%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+         int xor =0;
+        for(int i = 0 ;i < n ; i++){
+          int  num = start + 2 * i;
+             xor ^= num;
+        }
+        return xor;
+    }
+}
